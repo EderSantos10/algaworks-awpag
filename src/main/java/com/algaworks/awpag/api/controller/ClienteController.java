3100e0814661489fcc8db703bd9b6bd6474791bb
Eder Vinicius <eder.e10@hotmail.com>
2024-03-22 17:47:24 -0300

    Upadate Client and ClientController-Jakarta Persistence

--- src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
@@ -1,6 +1,5 @@
 package com.algaworks.awpag.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,25 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.awpag.domain.model.Cliente;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> Listar() {
+		return manager.createQuery( "from Cliente", Cliente.class).getResultList();
 		
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Joao");
-		cliente1.setEmail("joao@gmail.com");
-		cliente1.setTelefone("13-9999-1111");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setEmail("maria@gmail.com");
-		cliente2.setTelefone("13-9999-2222");
-		
-		return Arrays.asList(cliente1, cliente2);
 	}
 
 }
--- src/main/java/com/algaworks/awpag/domain/model/Cliente.java
+++ src/main/java/com/algaworks/awpag/domain/model/Cliente.java
@@ -1,16 +1,28 @@
 package com.algaworks.awpag.domain.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
 public class Cliente {
 
-	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
+	
+	@Column(name = "fone")
 	private String telefone;
 	
 }
