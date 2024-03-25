b88509a6ed1ab7afa938212b4144b1e942c6cbcc
Eder Vinicius <eder.e10@hotmail.com>
2024-03-25 15:56:50 -0300

    Update Exceptions

--- src/main/java/com/algaworks/awpag/api/controller/ParcelamentoController.java
+++ src/main/java/com/algaworks/awpag/api/controller/ParcelamentoController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algaworks.awpag.domain.exception.NegocioException;
 import com.algaworks.awpag.domain.model.Parcelamento;
 import com.algaworks.awpag.domain.repository.ParcelamentoRepository;
 import com.algaworks.awpag.domain.service.ParcelamentoService;
@@ -46,8 +44,4 @@ public class ParcelamentoController {
 		return parcelamentoService.cadastrar(parcelamento);
 	}
 	
-	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<String> capturar(NegocioException e) {
-		return ResponseEntity.badRequest().body(e.getMessage());
-	}
 }
